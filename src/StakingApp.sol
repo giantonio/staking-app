798abb38952fbd4ce7d79e2d705c5793b9a779a4
antonio <antoniogildiaz97@gmail.com>
2025-11-22 01:42:20 +0100

    All files

---
+++ src/StakingApp.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+contract StakingApp {
+
+    // 1. StakingToken address
+    // 2. Admin
+
+    // Variables
+    address public stakingToken;  
+
+    constructor(address stakingToken_, address admin){
+        stakingToken = stakingToken_;
+
+    }
+
+   
+}
